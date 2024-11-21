c9ec5b630218c9ea8ec81fdc920caee8ac111787
sblack-usu <sblack@cuahsi.org>
2024-11-21 13:15:05 -0700

    add stubbed endpoint for receiving access control changes from hydroshare

---
+++ api/routers/access_control_changed.py
@@ -0,0 +1,36 @@
+import logging
+from enum import Enum
+
+from fastapi import APIRouter
+from pydantic import BaseModel
+
+router = APIRouter()
+logger = logging.getLogger("micro-auth")
+
+
+class AccessLevel(Enum):
+    VIEW = "VIEW"
+    EDIT = "EDIT"
+    NONE = "NONE"
+
+
+class User(BaseModel):
+    username: str
+    access: AccessLevel
+
+
+class Resource(BaseModel):
+    id: str
+    public: bool
+    allow_private_sharing: bool
+    discoverable: bool
+    user_access: list[User]
+
+
+class AccessControlChangeRequest(BaseModel):
+    resources: list[Resource]
+
+
+@router.post("/user/resource/")
+async def save_access_key_argo(key_request: AccessControlChangeRequest):
+    print(f"Changing access control for {key_request.resources}")
